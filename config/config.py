6e23d015c79005ccee8799571245ebf3d3a9d4c2
iqjun <xgs2168@foxmail.com>
2017-10-12 23:26:43 +0800

    数据库操作

--- config/config.py
+++ config/config.py
@@ -1,1 +1,1 @@
-dbconfig=["host"]
+dbconfig={"host":"localhost","port":3306,"user":"root","password":"","db":"yii","charset":'utf8'}
--- pyMysql/mysqlDemo.py
+++ pyMysql/mysqlDemo.py
@@ -1,1 +1,22 @@
-import  pyMysql
+import pymysql
+from config.config import  dbconfig
+from pymysql.cursors import Cursor,SSCursor
+
+connection = pymysql.connect(**dbconfig)
+cursor = Cursor(connection)
+
+# 执行sql语句
+try:
+    with connection.cursor() as cursor:
+        # 执行sql语句，进行查询
+        sql = 'SELECT * from navbar where nav_id = 1'
+        cursor.execute(sql)
+        # 获取查询结果
+        result = cursor.fetchone()
+        print(result)
+    # 没有设置默认自动提交，需要主动提交，以保存所执行的语句
+    connection.commit()
+
+finally:
+    connection.close();
+
