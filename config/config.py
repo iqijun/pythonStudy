71bd2889d382735474acd1654edcc623f197a35f
iqjun <xgs2168@foxmail.com>
2017-10-15 22:53:42 +0800

    数据库操作

--- config/config.py
+++ config/config.py
@@ -1,1 +1,3 @@
-dbconfig={"host":"localhost","port":3306,"user":"root","password":"","db":"yii","charset":'utf8'}
+import  pymysql
+dbconfig = {"host": "localhost", "port": 3306, "user": "root", "password": "", "db": "yii", "charset": 'utf8',
+            "cursorclass": pymysql.cursors.DictCursor}
--- pyMysql/mysqlDemo.py
+++ pyMysql/mysqlDemo.py
@@ -1,22 +1,35 @@
 import pymysql
 from config.config import  dbconfig
-from pymysql.cursors import Cursor,SSCursor
+from pymysql.cursors import Cursor,SSCursor,DictCursor
 
 connection = pymysql.connect(**dbconfig)
-cursor = Cursor(connection)
+
 
 # 执行sql语句
+# try:
+#     with DictCursor(connection) as cursor:
+#         # 执行sql语句，进行查询
+#         sql = 'SELECT nav_id,nav_text from navbar where nav_id = 1'
+#         cursor.execute(sql)
+#         # 获取查询结果
+#         result = cursor.fetchone()
+#         print(result)
+#     # 没有设置默认自动提交，需要主动提交，以保存所执行的语句
+#     connection.commit()
+#
+# finally:
+#     connection.close();
+
 try:
-    with connection.cursor() as cursor:
+    with DictCursor(connection) as cursor:
         # 执行sql语句，进行查询
-        sql = 'SELECT * from navbar where nav_id = 1'
-        cursor.execute(sql)
+        sql = 'INSERT INTO navbar (nav_text,nav_url,nav_order)VALUES(%(text)s,%(url)s,%(order)s)'
+        cursor.execute(sql,{"text":"python","url":"/python","order":"10"})
         # 获取查询结果
-        result = cursor.fetchone()
-        print(result)
+        # result = cursor.fetchone()
+        # print(result)
     # 没有设置默认自动提交，需要主动提交，以保存所执行的语句
     connection.commit()
 
 finally:
     connection.close();
-
